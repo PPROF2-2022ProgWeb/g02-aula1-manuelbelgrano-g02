0320f0852c3d63d910e081b08a433fd506cad9f1
Amanda <92000104+amibiol@users.noreply.github.com>
2022-06-23 19:43:06 -0300

    Repositorio 2022 se actualiza js

---
+++ a1-g6-grupo-6-manuel-belgrano-main/a1-g6-grupo-6-manuel-belgrano-main/js/form.js
@@ -0,0 +1,71 @@
+const nombre = document.getElementById("myname");
+const apellidos = document.getElementById("surname");
+const correo = document.getElementById("email");
+const celular = document.getElementById("telefono");
+const contrasenia = document.getElementById("password");
+const contrasenia2 = document.getElementById("repetirPassword");
+const terminosYcondiciones = document.getElementById("terminosYcondiciones");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
+});
+
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
+    mostrarMensajeError("myname", "Nombre no valido*");
+    condicion = false;
+  }
+  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
+    mostrarMensajeError("surname", "Apellido no valido");
+    condicion = false;
+  }
+  if (correo.value.length < 1 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
+  if (
+    celular.value.length != 9 ||
+    celular.value.trim() == "" ||
+    isNaN(celular.value)
+  ) {
+    mostrarMensajeError("telefono", "Celular no valido*");
+    condicion = false;
+  }
+  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("password", "Contraseña no valido*");
+    condicion = false;
+  }
+  if (contrasenia2.value != contrasenia.value) {
+    mostrarMensajeError("repetirPassword", "Contraseña error*");
+    condicion = false;
+  }
+  if (!terminosYcondiciones.checked) {
+    mostrarMensajeError("terminosYcondiciones", "Acepte*");
+    condicion = false;
+  } else {
+    mostrarMensajeError("terminosYcondiciones", "");
+  }
+  return condicion;
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Listo !!";
+}
