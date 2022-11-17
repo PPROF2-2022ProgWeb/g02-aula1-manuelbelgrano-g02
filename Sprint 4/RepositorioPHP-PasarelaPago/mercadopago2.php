ea6a3e7104cdb73387591609668e4294a7df5c67
Carla <90620453+Cartaza@users.noreply.github.com>
2022-11-16 23:31:09 -0300

    Se agrega el repositorio PHP Pasarela de Pago.

---
+++ Sprint 4/RepositorioPHP-PasarelaPago/mercadopago2.php
@@ -0,0 +1,67 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-4076046132987876-110909-6eabb2cf87ba743789d48d700531380e-1235209127');
+
+
+$preference = new MercadoPago\Preference();
+
+
+//Suscripciòn anual
+$item3 = new MercadoPago\Item();
+$item3->id = '0003';
+$item3->title = 'Suscripciòn anual';
+$item3->quantity = 1;
+$item3->unit_price = 900;
+$item3->currency_id = 'ARS';
+
+
+$preference->items = array($item3);
+$preference->save();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrando Mercado pago</title>
+    <!-- SDK MercadoPago.js V2-->
+<script src="https://sdk.mercadopago.com/js/v2"></script>
+
+
+</head>
+<body>
+
+
+<div class="checkout-btn">
+
+</div>
+    
+
+<script>
+    const mp = new MercadoPago('APP_USR-60f8a6da-1bf2-4353-8037-087b87dc8096', {
+        locale: 'es-AR'
+    });
+
+    mp.checkout({
+        preference: {
+            id: '<?php echo $preference->id; ?>'
+        },
+        render: {
+            container: '.checkout-btn',
+            label: 'Pagar con Mercado Pago'
+        }
+    })
+    
+</script>
+</body>
+</html>
